62d2be395e7723dbd83f5f217264f12d343d7e71
GurTati <118075413+gurtatiLND@users.noreply.github.com>
2024-09-18 17:52:41 +0100

    fix-4567

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -7,37 +7,10 @@ module.exports = {
         if (!interaction.isChatInputCommand()) return;
             const command = interaction.client.commands.get(interaction.commandName);
 
-			if (!command) {
-				console.error(`No command matching ${interaction.commandName} was found.`);
-				return;
-			}
-
-			try {
-				await command.execute(interaction);
-			} catch (error) {
-				console.error(`Error executing ${interaction.commandName}`);
-				console.error(error);
-			}
-		} else if (interaction.isButton()) {
-		    
-			let responseMessage = '';
-            const customId = interaction.customId;
-
-			// Determine the response based on the button pressed
-			if (customId === 'ocean') {
-				responseMessage = 'You chose Ocean! Great choice!';
-			} else if (customId === 'mountains') {
-				responseMessage = 'You chose Mountains! Interesting choice!';
-			}
-
-			// Reply to the interaction with the response message
-			await interaction.reply({
-				content: responseMessage,
-				ephemeral: true // This ensures the response is visible only to the user who clicked the button
-			});
-	    }
-
-		
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
 		/////////// COOLDOWN BLOCK
 		const { cooldowns } = interaction.client;
 
@@ -74,5 +47,5 @@ module.exports = {
 				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 			}
 		}
-	}
+	},
 };
