b246ab98892bfa834f23ed1d1a10b31f3f8eab22
GurTati <118075413+gurtatiLND@users.noreply.github.com>
2024-09-18 17:28:44 +0100

    fix-145

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -63,7 +63,16 @@ module.exports = {
 		setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
 
 		////////////////////////////////
-	},
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+			} else {
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			}
+		}
+	}
 };
-
-
